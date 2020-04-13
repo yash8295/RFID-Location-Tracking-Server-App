bfbb0aef336d353069767f5dd0e34285733a5ddf
Yash Kashyap <yash.999kashyap@gmail.com>
2020-04-14 02:47:03 +0530

    First Commit

--- index.js
+++ index.js
@@ -108,11 +108,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.set('views', path.join(__dirname,'public'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname,'public')));
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
